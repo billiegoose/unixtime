ec2f4edd1ebfd3b1b25fcd4f8a2e8df4d31138ce
Will Hilton <wmhilton@gmail.com>
2017-03-12 00:30:58 -0500

    Actually set the CORS headers I intended...

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const cors = require('micro-cors')()
+const cors = require('micro-cors')({
+  allowMethods: ['GET', 'OPTIONS'],
+  allowHeaders: ['Cache-Control']
+})
 const handler = (req, res) => ((new Date()).valueOf() / 1000).toFixed(0)
 module.exports = cors(handler)
 
